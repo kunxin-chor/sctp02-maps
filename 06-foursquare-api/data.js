98e1b8af530adab3f16bc38bca5ecc2da5e0fa4e
kunxin-chor <chorkunxin@yahoo.com>
2024-02-07 06:29:57 +0000

    Added retrival of photos and added more interaction

--- 06-foursquare-api/data.js
+++ 06-foursquare-api/data.js
@@ -1,10 +1,13 @@
+const BASE_API_URL = "https://api.foursquare.com/v3";
+const API_KEY = "fsq32JqRqo6AHKIhCP2yosAeYOhHUDuoLuKTfgsOQmhStCY="
+
 async function search(lat, lng, searchTerms) {
-     const response = await axios.get('https://api.foursquare.com/v3/places/search', {
+    const response = await axios.get(`${BASE_API_URL}/places/search`, {
         params: {
             query: encodeURI(searchTerms), //encodeURI function to convert special characters to their encoded eqv so that query will be wellformed
             ll: lat + "," + lng,
             // categories:"13033",  // enable use of categories
-            sort:"DISTANCE",
+            sort: "DISTANCE",
             radius: 5000,
             limit: 50
 
@@ -12,12 +15,18 @@ async function search(lat, lng, searchTerms) {
         headers: {
             Accept: "application/json",
             // Provide the API key here
-            Authorization: 'fsq32JqRqo6AHKIhCP2yosAeYOhHUDuoLuKTfgsOQmhStCY='
+            Authorization: API_KEY
         }
     })
-   return response.data;
+    return response.data;
+}
 
-    
-
-
+async function getPhotoFromFourSquare(fsqId) {
+    const response = await axios.get(`${BASE_API_URL}/places/${fsqId}/photos`, {
+        headers: {
+            Accept: "application/json",
+            Authorization: API_KEY
+        }
+    });
+    return response.data;
 }
